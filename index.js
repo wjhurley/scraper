251738518966d486be779827184cf87a246ad374
whurley <whurley@velocity.org>
2020-01-15 18:19:48 -0800

    Add code to parse past games info

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ const cheerio = require('cheerio');
     .map(element => $(element).text())
     .filter(element => element.trim() !== '');
   console.log(gameDates);
+
+  const opponentTeamCompleted = $('#schedule-panel > table:nth-child(5) > tbody > tr:not(:nth-child(2)) > td:nth-child(5)')
+    .toArray()
+    .map(element => $(element).text())
+    .filter(element => element.trim() !== '');
+
+  const schoolArr = [];
+  const scoreArr = [];
+
+  opponentTeamCompleted.forEach(opponent => {
+    schoolArr.push(opponent.slice(0, -2).trim());
+    scoreArr.push(Number(opponent.replace(/[^\d]+/, '')));
+  });
+  console.log(schoolArr, scoreArr);
 })();
